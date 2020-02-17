f19ef8bf80d6c44ef824ba483d6bc2bbdcc5cc0c
José Hernández <foreveronline_bkn@hotmail.com>
2020-02-17 19:55:22 -0300

    estructuras primaris para consultas

--- server/src/controllers/jugadorController.ts
+++ server/src/controllers/jugadorController.ts
@@ -3,9 +3,24 @@ import pool from '../database';
 
 class JugadorController {
 
-    public index (req: Request, res: Response) {
-        // pool.query('DESCRIBE jugador');
-        res.json({text: 'API Is /jugador'});
+    public list (req: Request, res: Response) {
+        res.json({text: 'Listing jugador'});
+    }
+
+    public getOne (req: Request, res: Response) {
+        res.json({text: 'This is Jugador: ' + req.params.id });
+    }
+
+    public create (req: Request, res: Response) {
+        res.json({text: 'Creating jugador'});
+    }
+
+    public update (req: Request, res: Response) {
+        res.json({text: 'Updating jugador: ' + req.params.id });
+    }
+
+    public delete (req: Request, res: Response) {
+        res.json({text: 'Deleting jugador: ' + req.params.id });
     }
 }
 
--- server/src/routes/jugadoresRoutes.ts
+++ server/src/routes/jugadoresRoutes.ts
@@ -3,16 +3,20 @@ import { jugadorController } from '../controllers/jugadorController';
 
 class JugadoresRoutes {
 
-    public routes: Router = Router();
+    public router: Router = Router();
     constructor() {
         this.config();
     }
 
     config(): void {
-        this.routes.get('/', jugadorController.index);
+        this.router.get('/', jugadorController.list);
+        this.router.get('/', jugadorController.getOne);
+        this.router.post('/', jugadorController.create);
+        this.router.put('/:id', jugadorController.update);
+        this.router.delete('/:id', jugadorController.delete);
     }
 
 }
 
 const jugadoresRoutes = new JugadoresRoutes();
-export default jugadoresRoutes.routes;
+export default jugadoresRoutes.router;
